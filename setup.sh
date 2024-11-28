1d8018cf14a87848ca9099bece5686cec501d79b
Adrian Albu <2412rock@gmail.com>
2024-11-28 11:59:42 +0200

    first commit

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install nginx
+sudo systemctl enable nginx
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+curl -s https://install.zerotier.com | sudo bash
+sudo apt install zerotier-one
+sudo systemctl enable zerotier-one
+sudo systemctl start zerotier-one
