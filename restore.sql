cbf95f7357e8f433ce6fe5235f8c65c7a4390382
Adrian Albu <2412rock@gmail.com>
2025-02-12 20:25:14 +0200

    Update restore.sql

--- restore.sql
+++ restore.sql
@@ -1,6 +1,16 @@
-RESTORE DATABASE OverflowDB
-FROM DISK = '/var/opt/mssql/backup.bak'
-WITH MOVE 'OverflowDB' TO '/var/opt/mssql/data/OverflowDB.mdf',
-     MOVE 'OverflowDB_log' TO '/var/opt/mssql/data/OverflowDB_log.ldf',
+$containerName = "overflow-sql"
+$databaseName = "OverflowDB"
+$backupFile = "$env:USERPROFILE\Desktop\sql.bak"
+$containerBackupPath = "/var/opt/mssql/backup.bak"
+
+# Copy the backup file to the SQL container
+docker cp "$backupFile" "$containerName:$containerBackupPath"
+
+# Restore the database inside the container
+docker exec -i $containerName /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U SA -P "YourStrongPassword" -Q @"
+RESTORE DATABASE $databaseName
+FROM DISK = '$containerBackupPath'
+WITH MOVE '$databaseName' TO '/var/opt/mssql/data/OverflowDB.mdf',
+     MOVE '${databaseName}_log' TO '/var/opt/mssql/data/OverflowDB_log.ldf',
      RECOVERY;
-GO
+"@
